45c2a12dc26e5ebf26337094687c75fb6a3a218e
Mateusz Michalec <mateusz.michalec.pl@gmail.com>
2023-09-20 11:09:49 +0200

    deploy fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import store from "./app/store.js";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
+  <Router basename="/crypto">
     <Provider store={store}>
       <App />
     </Provider>
